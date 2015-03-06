cc50ee603a2b5636bc8b4547ecb78dd93f87fbe7
sameedali <sameed.ali.94@gmail.com>
2015-03-06 18:56:31 +0500

    vim update

--- s.sh
+++
@@ -1,8 +0,0 @@
-for i in $(ls -d bundle/*)
-    do
-        if [ -d "$i"/.git ]
-            then
-                echo $i
-                git submodule add $(cd $i && git remote show origin | grep Fetch | awk '{print $3}') ./$i
-        fi;
-done;
